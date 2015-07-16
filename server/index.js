000adce35577dab5d03f387c451e42e89e39c756
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-16 13:22:46 +0100

    Simple routing example

--- server/index.js
+++ server/index.js
@@ -3,12 +3,38 @@
 import Express from 'express';
 import path from 'path';
 import render from './render';
+import Router from 'routr';
+import create from './../app/modules/redux';
+import addTodo from '../app/modules/actions/add';
+import TodoStore from './../app/modules/stores/todos';
 
 const app = new Express();
+const router = new Router({
+  'view_user': {
+    path: '/user/:id',
+    method: 'get',
+    foo: {
+      bar: 'baz'
+    }
+  }
+});
 
 app.use('/build', Express.static(path.join(process.cwd(), 'build')));
+app.use('/api', (req, res) => {
+  const route = router.getRoute(req.url, {method: req.method});
+
+  if (route) {
+    redux.dispatch(addTodo('New thing'));
+    const html = render(redux);
+    res.send(html);
+  }
+  else {
+    res.send(404, '[Route not found]');
+  }
+});
 app.use((req, res) => {
-  const html = render();
+  const redux = create({todos: TodoStore});
+  const html = render(redux);
   res.send(html);
 });
 app.listen(8080, (err) => {
--- server/render.js
+++ server/render.js
@@ -1,14 +1,11 @@
 'use strict';
 
-import create from './../app/modules/redux';
 import React from 'react';
 import { Provider } from 'redux/react';
 import htmlComponent from '../app/modules/index';
 import TodoListContainer from '../app/modules/components/todoListContainer';
-import TodoStore from './../app/modules/stores/todos';
 
-const render = () => {
-  const redux = create({todos: TodoStore});
+const render = (redux) => {
   const markup = React.renderToString(
     <Provider redux={redux}>
       {()=><TodoListContainer/>}
